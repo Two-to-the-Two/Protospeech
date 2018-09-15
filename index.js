71ab9767dc0932af16abf4be1ad6aa7b022bdee0
Warren-Partridge <maps@perdix.org>
2018-09-15 15:36:07 -0400

    Add dialogflow post endpoint

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.post('/dialogflow', function (req, res) {
+  console.log('THRUSTERS FIRED');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
