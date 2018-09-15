2b9c140219880e3bda8a0ad20dd208adc79b3fc0
mhendrick98 <mhendric@bu.edu>
2018-09-15 15:46:53 -0400

    audio recorder

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var express = require('express');
+var express = require("express");
+var exphbs = require("express-handlebars");
 var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+app.use("/js", express.static("js"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", function(req, res) {
+  res.render("home");
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
 });
