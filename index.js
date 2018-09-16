cdf2cfa0ced2e875ea9afbf4f43e13c997309f59
mhendrick98 <mhendric@bu.edu>
2018-09-16 02:27:52 -0400

    yeeee

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ var https = require("https");
 var wav = require("wav");
 var exec = require("child_process").exec;
 
-const fs = require('fs');
-const speech = require('@google-cloud/speech');
+const fs = require("fs");
+const speech = require("@google-cloud/speech");
 
 const client = new speech.SpeechClient();
-
 
 const Lame = require("node-lame").Lame;
 var request = require("request");
@@ -123,58 +122,41 @@ binaryServer.on("connection", function(client) {
     stream.on("end", function() {
       fileWriter.end();
       console.log("wrote to file " + outFile);
-      const filename = "./demo.wav";
-      const encoding = 'LINEAR16';
-      const sampleRateHertz = 16000;
-      const languageCode = 'en-US';
+      setTimeout(function() {
+        console.log("begin GCP process");
+        const fileName = "./demo.wav";
 
-        const config = {
-            encoding: encoding,
-            sampleRateHertz: sampleRateHertz,
-            languageCode: languageCode,
-        };
+        // Reads a local audio file and converts it to base64
+        const file = fs.readFileSync(fileName);
+        const audioBytes = file.toString("base64");
         const audio = {
-            content: fs.readFileSync(filename).toString('base64'),
+          content: audioBytes
         };
-
+        console.log(audioBytes);
+        const config = {
+          encoding: "LINEAR16",
+          sampleRateHertz: 16000,
+          languageCode: "en-US"
+        };
+        console.log("config");
         const request = {
-            config: config,
-            audio: audio,
+          audio: audio,
+          config: config
         };
-
-// Detects speech in the audio file
         client
-            .recognize(request)
-            .then(data => {
-                const response = data[0];
-                const transcription = response.results
-                    .map(result => result.alternatives[0].transcript)
-                    .join('\n');
-                console.log(`Transcription: `, transcription);
-            })
-            .catch(err => {
-                console.error('ERROR:', err);
-            });
-    });
-/*
-    setTimeout(function() {
-      myheaders = {
-        Authorization:
-          "Bearer 01Ci_bKCjum7QV5VhzQjf2W-U1gJJBbVxiZPXy7X88yaU66AoTHiEeVGuRFrt7-C3jUAwdDXwY02qk2_RxL6GcqGq4LYg"
-      };
-      r = request
-        .get(
-          "https://api.rev.ai/revspeech/v1beta/jobs/" + x,
-          (headers = myheaders)
-        )
-        .on("response", function(response) {
-          console.log(response.statusCode); // 200
-          console.log(response.headers["content-type"]); // 'image/png'
-        });
-      console.log(r);
-    }, 10000);
-
-*/    /*
+          .recognize(request)
+          .then(data => {
+            const response = data[0];
+            const transcription = response.results
+              .map(result => result.alternatives[0].transcript)
+              .join("\n");
+            console.log(`Transcription: ${transcription}`);
+          })
+          .catch(err => {
+            console.error("ERROR:", err);
+          });
+      }, 5000);
+    }); /*
       request.post(
         {
           headers: {
@@ -192,5 +174,24 @@ binaryServer.on("connection", function(client) {
         }
       );
       */
+    /*
+    setTimeout(function() {
+      myheaders = {
+        Authorization:
+          "Bearer 01Ci_bKCjum7QV5VhzQjf2W-U1gJJBbVxiZPXy7X88yaU66AoTHiEeVGuRFrt7-C3jUAwdDXwY02qk2_RxL6GcqGq4LYg"
+      };
+      r = request
+        .get(
+          "https://api.rev.ai/revspeech/v1beta/jobs/" + x,
+          (headers = myheaders)
+        )
+        .on("response", function(response) {
+          console.log(response.statusCode); // 200
+          console.log(response.headers["content-type"]); // 'image/png'
+        });
+      console.log(r);
+    }, 10000);
+
+*/
   });
 });
